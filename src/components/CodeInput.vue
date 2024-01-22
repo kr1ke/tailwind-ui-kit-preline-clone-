2f63fa4b418097a161806605fa3644aa2e57070d
Илья Богданчиков <test200373@gmail.com>
2024-01-22 21:27:48 +0300

    feat: add code input

--- src/components/CodeInput.vue
+++ src/components/CodeInput.vue
@@ -1,45 +1,75 @@
 <template>
-<!--  <Codemirror-->
-<!--    v-model:value="code"-->
-<!--    :options="cmOptions"-->
-<!--    border-->
-<!--    placeholder="test placeholder"-->
-<!--    :height="200"-->
-<!--    @change="change"-->
-<!--  />-->
+  <Codemirror
+    v-model:value="code"
+    :options="cmOptions"
+    border
+    ref="cmRef"
+    @change="onChange"
+    @input="onInput"
+    @ready="onReady"
+  >
+  </Codemirror>
 </template>
-
 <script>
-// import Codemirror from "codemirror-editor-vue3";
-//
-// // placeholder
-// import "codemirror/addon/display/placeholder.js";
-//
-// // language
-// import "codemirror/mode/javascript/javascript.js";
-// // placeholder
-// import "codemirror/addon/display/placeholder.js";
-// // theme
-// import "codemirror/theme/dracula.css";
-//
-// import { ref } from "vue";
-// export default {
-//   components: { Codemirror },
-//   setup() {
-//     const code = ref(`
-// var i = 0;
-// for (; i < 9; i++) {
-//   console.log(i);
-//   // more statements
-// }`);
-//
-//     return {
-//       code,
-//       cmOptions: {
-//         mode: "text/javascript", // Language mode
-//         theme: "dracula", // Theme
-//       },
-//     };
-//   },
-// };
+import { ref, onMounted, onUnmounted } from "vue"
+import "codemirror/mode/vue/vue.js"
+import Codemirror from "codemirror-editor-vue3"
+import { oneDark } from '@codemirror/theme-one-dark'
+export default {
+  components: { Codemirror },
+  setup() {
+    const code = ref(
+      `var i = 0;
+for (; i < 9; i++) {
+    console.log(i);
+    // more statements
+}
+`
+    )
+
+    const cmRef = ref()
+    const cmOptions = {
+      mode: "text/x-vue",
+    }
+    const onChange = (val, cm) => {
+      console.log(val)
+      console.log(cm.getValue())
+    }
+
+    const onInput = (val) => {
+      console.log(val)
+    }
+
+    const onReady = (cm) => {
+      console.log(cm.focus())
+    }
+
+    onMounted(() => {
+      setTimeout(() => {
+        cmRef.value?.refresh()
+      }, 1000)
+
+      setTimeout(() => {
+        cmRef.value?.resize(300, 200)
+      }, 2000)
+
+      setTimeout(() => {
+        cmRef.value?.cminstance.isClean()
+      }, 3000)
+    })
+
+    onUnmounted(() => {
+      cmRef.value?.destroy()
+    })
+
+    return {
+      code,
+      cmRef,
+      cmOptions,
+      onChange,
+      onInput,
+      onReady
+    }
+  }
+}
 </script>
