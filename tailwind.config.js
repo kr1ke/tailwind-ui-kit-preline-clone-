e3996d1ec5cd714b28dbb7f5c10e95f1718fed3d
Илья Богданчиков <test200373@gmail.com>
2024-01-27 18:22:17 +0300

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  content: [
-    // './src/**/*.{html,js}',
-    'node_modules/preline/dist/*.js',
-  ],
-  plugins: [import('preline/plugin')],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', 'node_modules/preline/dist/*.js'],
+  plugins: [import('preline/plugin')]
 }
