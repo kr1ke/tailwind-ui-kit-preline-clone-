efc38566c40aa93e579230f215b0059e071d9e2e
Илья Богданчиков <test200373@gmail.com>
2024-04-20 22:54:44 +0300

    feat: get / set data from vue/repl example

--- src/components/BaseMonacoEditor.vue
+++ src/components/BaseMonacoEditor.vue
@@ -1,55 +1,37 @@
 <script setup>
-import { watchEffect, ref } from 'vue'
-import { Repl, useStore, useVueImportMap } from '@vue/repl'
+import '@vue/repl/style.css'
+import { Repl, useStore } from '@vue/repl'
 import CodeMirror from '@vue/repl/codemirror-editor'
+import { ref, watch } from 'vue'
 
-// retrieve some configuration options from the URL
-const query = new URLSearchParams(location.search)
+const code = ref(`
+      <template>
+        <h1>set data success</h1>
+      </template>`)
 
-const {
-  importMap: builtinImportMap,
-  vueVersion,
-  productionMode
-} = useVueImportMap({
-  // specify the default URL to import Vue runtime from in the sandbox
-  // default is the CDN link from jsdelivr.com with version matching Vue's version
-  // from peerDependency
-  runtimeDev: 'cdn link to vue.runtime.esm-browser.js',
-  runtimeProd: 'cdn link to vue.runtime.esm-browser.prod.js',
-  serverRenderer: 'cdn link to server-renderer.esm-browser.js'
-})
-
-const store = useStore(
-  {
-    // pre-set import map
-    builtinImportMap,
-    vueVersion,
-    // starts on the output pane (mobile only) if the URL has a showOutput query
-    showOutput: ref(query.has('showOutput')),
-    // starts on a different tab on the output pane if the URL has a outputMode query
-    // and default to the "preview" tab
-    outputMode: ref(query.get('outputMode') || 'preview')
-  },
-  // initialize repl with previously serialized state
-  location.hash
-)
-
-// persist state to URL hash
-watchEffect(() => history.replaceState({}, '', store.serialize()))
-
-// use a specific version of Vue
-vueVersion.value = '3.4.23'
-// production mode is enabled
-productionMode.value = true
-
-function test() {
-  store.setFiles({ 'src/App.vue': '<template><div>hello</div></template>' })
+const store = new useStore()
+function setData() {
+  store.setFiles({
+    'src/App.vue': code.value
+  })
+}
+function getFiles() {
+  console.log(store.getFiles())
 }
 </script>
 
 <template>
   <div>
-    <button @click="test">test</button>
-    <Repl :store="store" :editor="CodeMirror" />
+    <div class="flex *:border gap-2">
+      <button @click="setData">set data</button>
+      <button @click="getFiles">get data</button>
+    </div>
+    <Repl :editor="CodeMirror" :store="store" :clear-console="false" />
   </div>
 </template>
+
+<style>
+.editor {
+  min-height: 300px !important;
+}
+</style>
