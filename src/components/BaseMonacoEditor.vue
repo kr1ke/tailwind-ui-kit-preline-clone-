b886a43c856bae5e37be6f2de358bd700d77cbfa
Илья Богданчиков <test200373@gmail.com>
2024-04-23 10:12:34 +0300

    feat: set main example file & connect tailwind omMounted sandbox

--- src/components/BaseMonacoEditor.vue
+++ src/components/BaseMonacoEditor.vue
@@ -2,12 +2,15 @@
 import '@vue/repl/style.css'
 import { Repl, useStore } from '@vue/repl'
 import CodeMirror from '@vue/repl/codemirror-editor'
-import { ref, watch } from 'vue'
+import { computed, onMounted, ref, watch, watchEffect, nextTick } from 'vue'
+import appVue from './appVue.json'
+import exampleVue from './exampleVue.json'
 
-const code = ref(`
-      <template>
-        <h1>set data success</h1>
-      </template>`)
+const appVueCode = appVue.code
+const exampleCode = exampleVue.code
+console.log(appVueCode, exampleCode)
+
+const replComponent = ref()
 
 const store = new useStore({
   builtinImportMap: ref({
@@ -18,27 +21,73 @@ const store = new useStore({
     }
   })
 })
-function setData() {
-  store.setFiles({
-    'src/App.vue': code.value
+async function initEditor() {
+  await store.setFiles({
+    'src/App.vue': appVueCode,
+    'src/Example.vue': exampleCode
   })
+  await store.setActive('src/Example.vue')
+  // console.log(store.getFiles())
+  setTimeout(() => {
+    store.init()
+  }, 600)
+}
+onMounted(async () => {
+  await initEditor()
+})
+function setData() {
+  // store.setFiles({
+  //   'src/App.vue': code.value,
+  //   'src/index.vue': code.value
+  // })
 }
 function getFiles() {
+  console.log(replComponent.value)
+  console.log(store.loading)
   console.log(store.getFiles())
 }
 function onSetImportMap() {
   // console.log(store)
 }
+
+function test() {
+  console.log('test')
+}
+
+const counter = ref(0)
+
+// watch(
+//   () => store,
+//   async (newValue, oldValue) => {
+//     console.log(newValue, oldValue)
+//     counter.value++
+//     if (counter.value === 7) {
+//       await nextTick()
+//       setTimeout(() => {
+//         console.log('hhhh')
+//         store.init()
+//       }, 1)
+//     }
+//   },
+//   { deep: true }
+// )
 </script>
 
 <template>
   <div>
     <div class="flex *:border gap-2">
+      {{ counter }}
       <button @click="onSetImportMap">click</button>
       <button @click="setData">set data</button>
       <button @click="getFiles">get data</button>
     </div>
-    <Repl :editor="CodeMirror" :store="store" :clear-console="false" />
+    <Repl
+      :editor="CodeMirror"
+      @update="test"
+      :store="store"
+      :clear-console="false"
+      ref="replComponent"
+    />
   </div>
 </template>
 
